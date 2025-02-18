54af6298e5eb9ddd99b6e92c9fee76430ea7b8eb
BrunoElombo <brunoelombo@gmail.com>
2025-02-18 10:59:57 +0100

    add cors configuration

--- src/middlewares/verifyToken.middleware.js
+++ src/middlewares/verifyToken.middleware.js
@@ -34,9 +34,8 @@ export const verifyUserExist = async (req, res, next) => {
         if(!decodedToken) return sendStatus(HTTP_STATUS.UN_AUTHORIZED.statusCode);
 
         let employee = await getEmployee(decodedToken?.user_id, token);
-
-        if(employee?.data.length <1) return res.sendStatus(HTTP_STATUS.UN_AUTHORIZED.statusCode);
-
+        if(!employee?.id) return res.sendStatus(HTTP_STATUS.UN_AUTHORIZED.statusCode);
+        
         if(req.method === "POST"){
             req.body["createdBy"] = employee?.id;
         }else if(req.method === "PATCH" || req.method === "DELETE"){
