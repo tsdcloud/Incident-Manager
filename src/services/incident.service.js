48c3db3ffd8c69b5509f15ae47daf4c13ec8ea87
BrunoElombo <brunoelombo@gmail.com>
2025-05-09 15:35:11 +0200

    fix count total incidents

--- src/services/incident.service.js
+++ src/services/incident.service.js
@@ -64,7 +64,7 @@ export const getAllIncidentService = async() =>{
                 creationDate:'desc'
             }
         });
-        const total = incidents.length;
+        const total = await incidentClient.count({where:{isActive:true}});
         return {
             page: parseInt(page),
             totalPages: Math.ceil(total / LIMIT),
